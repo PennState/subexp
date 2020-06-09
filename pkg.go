92eb999808fa5abf7091e0686ab5cdcd8c3d907b
Steve Moyer <smoyer@psu.edu>
2020-06-09 16:21:18 -0400

    Adds package documentation

---
+++ pkg.go
@@ -0,0 +1,12 @@
+/*
+Package subexp provides extensions to the Go regexp standard library
+to make using sub-expressions easier.  Other regular expression
+documentation refers to these expressions as "named capture groups",
+so some of the methods included with this package use those terms
+as their names.
+
+For convenience, compilation of the regular expressions is wrapped
+along with the capture functionality so that, in simple cases, import
+of the Go regexp standard library may not be needed.
+*/
+package subexp
